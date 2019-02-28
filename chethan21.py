49ad37091e0f7aaf7bce0465a0c2c58ad56cb307
Chethanmanu <48007152+Chethanmanu@users.noreply.github.com>
2019-02-28 14:53:37 +0530

    Add files via upload

--- chethan21.py
+++ chethan21.py
@@ -1,20 +1,46 @@
 import random
 l=["r","p","s"]
+d={'r':'rock','p':'paper','s':'scissor'}
+us=0
+cs=0
 while True:
 	#take input from user
 	u=input("enter your choice ,press n to discontinue")
 	#to exit
 	if u=='n':
 		print("game over")
+		print("computer score:",cs)
+		print("user score",us)
+		if cs==us:
+		    print("tie")
+		elif cs > us:
+			print("computer wins")
+		else:
+			print("user wins")
 		exit()
 	#input from computer
 	c=random.choice(l)
 	print ("computer chooses",c)
-
-	#compare the user and computer choice 
+    #compare the user and computer choice 
 	if u == c:
 		print("tie")
-	elif u=="r" and c=="p":
+
+	elif u=='r' and c=='p':
 		print("comp wins")
-	elif u == 'r' and c =='s':
+		cs+=1
+	elif u=='r' and c=='s':
 		 print("user wins")
+		 us+=1
+
+	elif u=='p' and c=='r':
+		print("comp wins")
+		us+=1
+	elif u=='p' and c=='s':
+		print("user wins")
+		us+=1
+	elif u=='s' and c=='r':
+	     print("comp wins")
+	     cs+=1
+	elif u=='s' and c=='p':
+	     print("user wins")
+	     us+=1
